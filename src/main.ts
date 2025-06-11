e0bec79dafebe6f779aef086afd536bfa8196302
Densodim <25denso@gmail.com>
2025-06-11 16:23:43 +0300

    make API

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,22 @@ import { swaggerConfig, swaggerCustomOptions } from "./config/swagger.config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup("api", app, document, swaggerCustomOptions);
+  // Настройка CORS
+  app.enableCors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
 
-  await app.listen(process.env.PORT ?? 3001);
+  // Глобальный префикс API
+  app.setGlobalPrefix("api");
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("docs", app, document, swaggerCustomOptions);
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
