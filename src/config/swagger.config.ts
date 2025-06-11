1d73f41e5abd25ad9ecaa43d46647ba269c4aa43
Densodim <25denso@gmail.com>
2025-06-11 14:50:06 +0300

    create swagger config

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,28 @@
+import { DocumentBuilder, SwaggerCustomOptions } from "@nestjs/swagger";
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle("Form")
+  .setDescription("The Form API description")
+  .setVersion("1.0")
+  .build();
+
+export const swaggerCustomOptions: SwaggerCustomOptions = {
+  swaggerOptions: {
+    persistAuthorization: true,
+    docExpansion: "none",
+    filter: true,
+    showCommonExtensions: true,
+    syntaxHighlight: {
+      theme: "monokai",
+    },
+  },
+  customSiteTitle: "Form API Documentation",
+  customfavIcon: "https://nestjs.com/img/favicon.ico",
+  customJs: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js",
+  ],
+  customCssUrl: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css",
+  ],
+};
